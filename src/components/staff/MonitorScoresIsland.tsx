f289a9b12a59f8365dc059f96baefcf8dc35829f
nseobong69 <nseobongedem@gmail.com>
2026-09-12 12:22:51 +0100

    Wrap MonitorScores in ToastProvider

--- src/components/staff/MonitorScoresIsland.tsx
+++ src/components/staff/MonitorScoresIsland.tsx
@@ -1,1 +1,16 @@
+import { ToastProvider } from '../ui/Toast';
+import MonitorScores from './MonitorScores';
+
+interface Props {
+  role: string;
+  userId: string;
+}
+
+export default function MonitorScoresIsland(props: Props) {
+  return (
+    <ToastProvider>
+      <MonitorScores {...props} />
+    </ToastProvider>
+  );
+}
 
